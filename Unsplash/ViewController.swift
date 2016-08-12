bde3d475f5b8a1edbc8b57a308f8f25ac603fb47
Michael Auer <mauermcx@gmail.com>
2016-08-12 00:57:39 -0400

    Grab a single image url from unsplash API

--- Unsplash/ViewController.swift
+++ Unsplash/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = "https://httpbin.org/ip"
+        let url = "https://api.unsplash.com/photos/?client_id=" + Secret().unsplashClientID
         Alamofire.request(.GET, url).validate().responseJSON { response in
             switch response.result {
             case .Success:
                 if let value = response.result.value {
                     let json = JSON(value)
-                    print("JSON: \(json)")
+                    print ((json.first?.1["urls"]["thumb"].string)!)
                 }
             case .Failure(let error):
                 print(error)
